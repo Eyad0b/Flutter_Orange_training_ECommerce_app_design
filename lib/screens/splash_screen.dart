dfcf2a6aa61fe7dadc324c8e10682fa6e093e39f
Eyad0b <110825146+Eyad0b@users.noreply.github.com>
2023-06-21 22:41:22 +0300

    Complete signUp and login screens

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -20,7 +20,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
-    
+
     return SafeArea(
       child: Scaffold(
         resizeToAvoidBottomInset: false,
